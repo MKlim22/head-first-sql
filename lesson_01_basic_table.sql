59f71c8a05b1f023acb76faa4596ce3a5f49b0fb
Magda <mklimczak05@gmail.com>
2026-01-13 18:33:47 +0100

    Small improvements in Lesson 01 comments

--- lesson_01_basic_table.sql
+++ lesson_01_basic_table.sql
@@ -1,11 +1,15 @@
 -- Lesson 1: Basic table creation
+-- This skript can be run multiple times without errors
 
 CREATE DATABASE IF EXISTS gregs_list;
 USE gregs_list;
 
 -- Remove table if it already exists
+
 DROP TABLE IF EXISTS my_contacts;
+
 -- Create the my_contacts table
+
 CREATE TABLE my_contacts
 (
 last_name VARCHAR(30),
@@ -18,6 +22,8 @@ status VARCHAR(20),
 interests VARCHAR(100),
 seeking VARCHAR(100)
 );
+
 -- Verify the table creation
+
 SHOW TABLES;
 DESC my_contacts;
