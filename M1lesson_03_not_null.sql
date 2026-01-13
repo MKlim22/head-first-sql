e2ad2d295c5d28010421ecbf3933e04beab91029
Magda <mklimczak05@gmail.com>
2026-01-13 19:02:54 +0100

    Lesson 03: NOT NULL and INSERT statement

---
+++ M1lesson_03_not_null.sql
@@ -0,0 +1,29 @@
+-- Lesson 3: NOT NULL Constraint and insert Statement
+
+CREATE DATABASE IF NOT EXISTS greg_list;
+USE greg_list;
+DROP TABLE IF EXISTS my_contacts;
+
+CREATE TABLE my_contacts
+(
+last_name VARCHAR(30) NOT NULL,
+first_name VARCHAR(20) NOT NULL,
+email VARCHAR(50) NOT NULL,
+gender CHAR(1) NOT NULL,
+birthday DATE NOT NULL,
+-- In this exercise, birthday is required (book example)
+profession VARCHAR(50) NOT NULL,
+location VARCHAR(50) NOT NULL,
+status VARCHAR(20) NOT NULL,
+interests VARCHAR(100) NOT NULL,
+seeking VARCHAR(100) NOT NULL
+);
+
+INSERT INTO my_contacts
+(last_name, first_name, email, gender, birthday, profession, location, status, interests, seeking)
+VALUES
+('Andersen','Jilian','jill_andersen@breakneckpizza.com','F','1980-09-05','Technical writer','Palo Alto, CA','Single','Kayaking,Reptiles','Relationship,Friends');
+
+-- Verify the inserted data
+
+SELECT * FROM my_contacts;
