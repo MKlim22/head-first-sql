e8b949f8d241bf9dd75efcf51e992c0cf0519597
Magdalena <mklimczak05@gmail.com>
2026-01-22 22:28:07 +0100

    Add SQL queries for various WHERE conditions

--- M2/queries.sql
+++ M2/queries.sql
@@ -16,3 +16,16 @@ WHERE main = 'soda' AND amount2 > 1;
 -- 5) Not equal
 SELECT drink_name FROM easy_drinks
 WHERE main <> 'soda';
+
+-- 6) WHERE with OR
+SELECT drink_name
+FROM easy_drinks
+WHERE main = 'cherry juice'
+OR second = 'cherry juice';
+
+-- 7) WHERE with AND vs OR (same column, different values)
+-- Correct usage with OR
+SELECT drink_name
+FROM easy_drinks
+WHERE main = 'orange juice'
+OR main = 'apple juice';
